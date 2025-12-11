3870ca0677668a8a55e3f3036bafe3247a3cf88e
Pawan kumar <pawankr16123114@gmail.com>
2025-12-12 00:38:32 +0530

    Time: 0 ms (100%), Space: 23.7 MB (81.65%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int j = 0;
+        for (int x : nums)
+            if (x != 0) nums[j++] = x;
+        while (j < nums.size())
+            nums[j++] = 0;
+    }
+};
