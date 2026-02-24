4f2d9b74554ea63f15fb0542962d299525fe65a4
Pawan kumar <pawankr16123114@gmail.com>
2026-02-24 21:17:43 +0530

    Time: 3 ms (45.33%), Space: 8.6 MB (65.53%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0) return false;
+        long long reverse = 0;
+        int original = x;
+        while(original!=0){
+            reverse = reverse*10 + original%10;
+            original /=10;
+        }
+        return reverse == x;
+    }
+};
