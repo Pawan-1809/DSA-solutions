35f73ce7369c8049c0a2d030d0414a6c43c52e83
Pawan kumar <pawankr16123114@gmail.com>
2026-07-24 01:54:35 +0530

    Time: 11 ms (49.62%), Space: 31.1 MB (18.82%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.cpp
+++ 0221-maximal-square/0221-maximal-square.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int maximalSquare(vector<vector<char>>& matrix) {
+
+        // Kisi bhi index pe agar uske diagonal element , upar aur bagal ka element agar 1 h to wo sab square banayenge har size ke square ke liye ye valid h, therefore hum dp matrix me wahi size dekh ke update karenge agar ek bhi 0 h to wo square possible nhi h (Strength of a group is equal to the weakest in the group)
         int n = matrix.size();
         int m = matrix[0].size();
 
