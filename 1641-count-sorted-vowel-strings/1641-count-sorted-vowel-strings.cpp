7f084c861b00230ccac4fe833b1a3f69e1e8b34e
Pawan kumar <pawankr16123114@gmail.com>
2026-07-24 09:26:26 +0530

    Time: 0 ms (100%), Space: 7.6 MB (98.42%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
@@ -1,29 +1,16 @@
 class Solution {
 public:
-    int countVowelStrings(int n) {
-        //Sorted order ki wajah se har vowel sirf apne ya uske baad wale vowels se hi continue ho sakta hai. Isliye reverse cumulative DP lagti hai: u=u, o=o+u, i=i+o+u, e=e+i+o+u, a=a+e+i+o+u; har iteration me ye counts update karte hain aur end me sab states ka sum answer hota hai
-        int five = 1;
-        int four = 0;
-        int three = 0;
-        int two = 0;
-        int one = 0;
-                
-        for(int i = 1;i < n; i++){
-            int nfive = five;
-            int nfour = five + four;
-            int nthree = five + four+ three;
-            int ntwo = five + four + three + two;
-            int none = five + four + three + two + one;
+    int countVowelStrings(int n) { 
+        //kyunki lexicographical order maintain karna hai, isliye a ke baad sab vowels aa sakte hain, e ke baad sirf e,i,o,u, i ke baad i,o,u, o ke baad o,u, aur u ke baad sirf u, isliye har iteration me a=a+e+i+o+u, e=e+i+o+u, i=i+o+u, o=o+u, u=u update karte hain aur end me a+e+i+o+u hi total valid sorted vowel strings ka answer hota hai.
+        int a = 1, e = 1, i = 1, o = 1, u = 1; 
 
-            five = nfive;
-            four = nfour;
-            three = nthree;
-            two = ntwo;
-            one = none;
-            
-            
+        for(int j = 1; j < n ; j++){
+            a = a + e + i + o + u;
+            e = e + i + o + u;
+            i = i + o + u;
+            o = o + u;
+            u = u;
         }
-        long long ans = one + 2*two + 3*three + 4*four + 5*five;
-        return ans;
+        return a+e+i+o+u;
     }
 };
