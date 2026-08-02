96e3a3f230df458cdade9b2ae4e0747fa9df974d
Pawan kumar <pawankr16123114@gmail.com>
2026-08-02 10:25:47 +0530

    Time: 0 ms (100%), Space: 10.3 MB (16.32%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
+    int f(vector<int>& arr, int left, int right ,vector<vector<int>>& memo) {
+        if (left > right)
+            return 0;
+        if (memo[left][right] != INT_MIN)
+            return memo[left][right];
+        int choseLeft = arr[left] - f(arr, left + 1, right, memo);
+        int choseRight = arr[right] - f(arr, left, right - 1, memo);
+
+        return memo[left][right] = max(choseLeft, choseRight);
+    };
+
+
     bool predictTheWinner(vector<int>& nums) {
         int n = nums.size();
-        if (n == 0) return true;
-        vector<int> dp(n, 0);
-        
-        for (int i = 0; i < n; i++) dp[i] = nums[i];
-        
-        for (int i = n - 2; i >= 0; i--) {
-            for (int j = i + 1; j < n; j++) {
-                dp[j] = max(nums[i] - dp[j], nums[j] - dp[j - 1]);
-            }
-        }
-        
-        return dp[n - 1] >= 0;
+        vector<vector<int>> memo(n, vector<int>(n, INT_MIN));
+        return f(nums, 0, n - 1, memo) >= 0;
     }
 };
