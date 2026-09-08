33f4577b8635b827f97182018265e1116613eeff
Pawan kumar <pawankr16123114@gmail.com>
2026-09-08 15:37:32 +0530

    Time: 0 ms (100%), Space: 8.7 MB (54.49%) - LeetHub

--- 3945-digit-frequency-score/3945-digit-frequency-score.cpp
+++ 3945-digit-frequency-score/3945-digit-frequency-score.cpp
@@ -1,15 +1,12 @@
 class Solution {
 public:
     int digitFrequencyScore(int n) {
-        unordered_map<int, int> freq;
+        
+        int ans = 0;
         while(n>0){
             int digit = n%10;
-            freq[digit]++;
+            ans+=digit;
             n /= 10;
-        }
-        int ans = 0;
-        for(auto& it : freq){
-            ans+= it.first * it.second;
         }
         return ans;
     }
